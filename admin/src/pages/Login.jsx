a28db1ed13dea7ecb90e79e5d0c022a0d3f298f3
Mohit6261 <105044988+Mohit6261@users.noreply.github.com>
2024-11-27 18:09:16 +0530

    Update Login.jsx

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -53,12 +53,12 @@ const Login = () => {
 
             <div className='w-full'>
                 <p>Email</p>
-                <input onChange={(e)=>setEmail(e.target.value)} value={email} className='border border-[#DADADA] rounded w-full p-2 mt-1' type="email" required />
+                <input placeholder='admin@gmail.com' onChange={(e)=>setEmail(e.target.value)} value={email} className='border border-[#DADADA] rounded w-full p-2 mt-1' type="email" required />
             </div>
 
             <div className='w-full'>
                 <p>Password</p>
-                <input onChange={(e)=>setPassword(e.target.value)} value={password}  className='border border-[#DADADA] rounded w-full p-2 mt-1' type="password" required />
+                <input placeholder='admin123' onChange={(e)=>setPassword(e.target.value)} value={password}  className='border border-[#DADADA] rounded w-full p-2 mt-1' type="password" required />
             </div>
             <button className='bg-primary text-white w-full py-2 rounded-md text-base'>Login</button>
 
